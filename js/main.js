a516787fa900b49d22aab14e9be5aa8aed7a631b
mdss@makexhtml.ru <mdss@makexhtml.ru>
2017-09-26 01:38:21 +0300

    final version

---
+++ js/main.js
@@ -0,0 +1,23 @@
+(function($){ 
+	"use strict";
+
+	$(window).on('load resize', function() {
+		if ($(window).width() < 800) {
+			if (!$('.nav').has('.nav__mobile').length) $('.nav').wrapInner('<div class="nav__mobile"></div>');
+			$('html').click(function() {
+				$('.nav__mobile--active').removeClass('nav__mobile--active');
+			});
+			$('.burger').click(function(e) {
+				e.preventDefault();
+				e.stopPropagation();
+				$('.nav__mobile').toggleClass('nav__mobile--active');
+			});
+			$('.nav__mobile--active').click(function(e) {
+				e.stopPropagation();
+			});
+		}
+	});
+
+	console.log('%c Верстка: mdss@makexhtml.ru ', 'color:#fff;font-size:1.2rem;background-color:#3469c6;')
+
+})(jQuery);
